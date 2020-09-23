00d1d46167ba83691303b01f13df6b7853ded0e3
hozquar <hozquar@gmail.com>
2020-09-23 08:10:13 -0500

    agregado el map, falta mejorar el ubication detail

--- src/main/java/com/example/platziconf/view/ui/fragment/UbicationFragment.kt
+++ src/main/java/com/example/platziconf/view/ui/fragment/UbicationFragment.kt
@@ -1,60 +1,62 @@
 package com.example.platziconf.view.ui.fragment
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.example.platziconf.R
+import com.example.platziconf.model.Ubication
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UbicationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UbicationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+class UbicationFragment : Fragment(), OnMapReadyCallback , GoogleMap.OnMarkerClickListener  {
+    //se añade OnMapreadycalback para sincronizar mapa y manipularlo en cualquier momento
+    // se añade OnMarkerListener para al dar cllick se valla a al frargmenubicationdetail
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_ubication, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UbicationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UbicationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment= childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        //chilfrgament es el hijo del que controla el map y se le da el identificador para hacerlo atravez de un fragmet
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {//aqui ya aparece el mapa en adelante lo modificamos
+        val ubication = Ubication()
+        val zoom= 16f
+        val centerMap = LatLng(ubication.latitud,ubication.longitud)//variable de ubicacion en el mapa
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap,zoom))
+        val centerMark = LatLng(ubication.latitud,ubication.longitud)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+        markerOptions.title("Platzi Conf 2019")
+
+        val bitmapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.logo_platzi) } as BitmapDrawable
+        //se añade la imagen marcador de el lugar del mapa
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150 , 150,false)
+        //modifico el tamaño de la imagen
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+        //carga la imagen como icono
+        googleMap?.addMarker(markerOptions) //coloca LA IMAGEN EN EL MAPA
+        googleMap?.setOnMarkerClickListener(this)
+       }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailFragmentDialog)
+        return true
     }
 }
